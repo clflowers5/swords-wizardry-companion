c4869313578b6bae3079346d5d2a2bb213d4cccb
Chris Flowers <clflowers5@gmail.com>
2020-08-06 19:05:50 -0700

    convert to tailwind

--- components/layout.js
+++ components/layout.js
@@ -1,18 +1,17 @@
 import React from "react";
 import Head from "next/head";
-import { Container, Heading } from "react-bulma-components";
 
 function Layout({ children }) {
   return (
-    <Container>
+    <div className="container mx-auto">
       <Head>
         <title>Swords & Wizardry Spells</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Swords & Wizardry Spells" />
       </Head>
-      <Heading>Swords & Wizardry Spell List</Heading>
-      <main>{children}</main>
-    </Container>
+      <h1 className="font-bold text-center">Swords & Wizardry Spell List</h1>
+      <main className="mt-4">{children}</main>
+    </div>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Box } from "react-bulma-components";
 
 import Layout from "../components/layout";
 import { getSpells } from "../lib/spells/spells";
@@ -8,30 +7,44 @@ import { getSpells } from "../lib/spells/spells";
 function Home({ spellList }) {
   return (
     <Layout>
-      <div>
-        {spellList.map((spell) => (
-          <Box key={spell.title}>
-            <div>{spell.title}</div>
+      {spellList.map((spell) => (
+        <div
+          className="bg-white rounded overflow-hidden shadow-md border-solid border border-gray-200 p-3 mb-4"
+          key={spell.title}
+        >
+          <h5 className="font-medium underline">{spell.title}</h5>
 
+          <div>
             <div>
-              <div>Learned by:</div>
-              {spell.learnedBy.map((profession) => (
-                <div key={profession.className}>
-                  {profession.className}: {profession.level}
-                </div>
+              <span>Learned by:</span>
+              {spell.learnedBy.map((profession, index) => (
+                <span key={profession.className}>
+                  {index > 0 ? ", " : " "}
+                  {profession.className} - {profession.level}
+                </span>
               ))}
             </div>
+          </div>
 
-            <div>Range: {spell.range}</div>
-            <div>Duration: {spell.duration}</div>
-            <div>
-              {spell.description.map((entry, index) => (
-                <div key={index}>{entry}</div>
-              ))}
-            </div>
-          </Box>
-        ))}
-      </div>
+          <div>Range: {spell.range}</div>
+
+          <div>Duration: {spell.duration}</div>
+
+          <div className="mt-3">
+            {spell.description.map((entry, index) => (
+              <div key={index}>{entry}</div>
+            ))}
+          </div>
+
+          {/* It's not pretty, but boy does it work */}
+          {spell.table && (
+            <div
+              className="mt-4"
+              dangerouslySetInnerHTML={{ __html: spell.table }}
+            />
+          )}
+        </div>
+      ))}
     </Layout>
   );
 }
