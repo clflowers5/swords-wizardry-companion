585729e130215c2c174a8b42ad5b5d11a3c6869d
Chris Flowers <clflowers5@gmail.com>
2020-10-06 07:58:23 -0700

    WIP - fix(Bookmarks): Fix search results displaying when no favorites are s… (#13)
    * fix(Bookmarks): Fix search results displaying when no favorites are selected with favorite search togged on
    
    * fix(BookmarkedSpells): Add debug module with some logs, and fix bookmarked spells not rendering correctly in all cases.

--- components/spell-list-search-container.js
+++ components/spell-list-search-container.js
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import Fuse from "fuse.js";
 import PropTypes from "prop-types";
+import debug from "debug";
 
 // Components
 import CheckBox from "components/inputs/checkbox";
@@ -14,6 +15,8 @@ import useDebounce from "hooks/use-debounce";
 
 // Utils
 import buildSpellMap from "utils/build-spell-map";
+
+const log = debug('spell-list-search-container');
 
 function SpellListSearchContainer({
   bookmarkedSpells,
@@ -45,8 +48,7 @@ function SpellListSearchContainer({
 
   useEffect(
     function setupFuse() {
-      const spells = filteredSpellList.length ? filteredSpellList : spellList;
-      fuseRef.current = new Fuse(spells, {
+      fuseRef.current = new Fuse(filteredSpellList, {
         keys: ["title", "learnedBy.className"],
         minMatchCharLength: 2,
         threshold: 0.4,
@@ -109,6 +111,9 @@ function SpellListSearchContainer({
     175,
     [searchValue]
   );
+
+  log('Rendering searchResults', searchResults);
+  log('Rendering filteredSpellList', filteredSpellList);
 
   return (
     <Layout>
